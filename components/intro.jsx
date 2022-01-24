f71257af6ba8c5a1074276715c50ef9ce28ecfdd
rospisey <rospisey9999@gmail.com>
2022-01-24 20:05:00 +0700

    landing page prototyping responsive

--- components/intro.jsx
+++ components/intro.jsx
@@ -1,9 +1,21 @@
 import styles from "../styles/Intro.module.css";
+import Image from "next/image";
+import Circle from "./circle";
 const Intro = () => {
   return (
     <div className={styles.container}>
-      <div className={styles.card}>first</div>
-      <div className={styles.card}>Second</div>
+      <Circle backgroundColor="yellow" top="-50vh" left="-50vh"/>
+      <Circle backgroundColor="green" right="-40vh"/>
+      <div className={styles.card}>
+        <h1 className={styles.title}>
+        <span className={styles.brand}>PANHCHANA</span> WEB DEVELOPMENT
+        </h1>
+        <p className={styles.desc}>Develop your ideas to the world by our professional team to grow your bussiness</p>
+        <button className={styles.button}>DISCOVER</button>
+      </div>
+      <div className={styles.card}>
+        <Image src="/landing.jpg" height="100%" width="100%" layout="fill" objectFit="contain" alt=""/>
+      </div>
     </div>
   );
 };
